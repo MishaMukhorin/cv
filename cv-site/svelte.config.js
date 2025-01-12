2bce7b16e292edc7ea42dee2d97a60b24efb7d52
Misha.Mukhorin <misha.mukhorin@icloud.com>
2025-01-12 17:53:08 +0300

    pages4

--- cv-site/svelte.config.js
+++ cv-site/svelte.config.js
@@ -3,20 +3,14 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    paths: {
-      base: process.env.NODE_ENV === 'production' ? '/repository-name' : '',
-    },
-    adapter: {
-      name: 'adapter-static',
-      options: {
-        pages: 'build',
-        assets: 'build',
-        fallback: null,
-      },
-    },
-  },
-  preprocess: sveltePreprocess(),
+	preprocess: vitePreprocess(),
+	kit: {
+
+		adapter: adapter(),
+		paths: {
+            base: '',
+        },
+	}
 };
 
 export default config;
