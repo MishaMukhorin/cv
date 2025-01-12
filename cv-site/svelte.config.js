275f22814608dc2681745021f9faf42742120878
Misha.Mukhorin <misha.mukhorin@icloud.com>
2025-01-12 17:45:08 +0300

    pages4

--- cv-site/svelte.config.js
+++ cv-site/svelte.config.js
@@ -3,14 +3,22 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-
-		adapter: adapter(),
-		paths: {
-            base: '/${hello}',
-        },
-	}
+  kit: {
+    // This is important to set the base path when deployed to GitHub Pages
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/repository-name' : ''
+    },
+    // Add the adapter to deploy to GitHub Pages
+    adapter: {
+      name: 'adapter-static',
+      options: {
+        pages: 'build',
+        assets: 'build',
+        fallback: null,
+      },
+    },
+  },
+  preprocess: sveltePreprocess(),
 };
 
 export default config;
