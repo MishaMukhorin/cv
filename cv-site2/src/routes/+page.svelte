e8b80f533373646d31a48298f204341a10d91b34
Misha.Mukhorin <misha.mukhorin@icloud.com>
2025-01-12 22:35:16 +0300

    deploy

--- cv-site2/src/routes/+page.svelte
+++ cv-site2/src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script>
   // No script logic is needed unless you want to add interactivity
 </script>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
 
 <style>
   body {
-    font-family: Arial, sans-serif;
+    font-family: Poppins, sans-serif;
     line-height: 1.6;
     margin: 0;
     padding: 0;
